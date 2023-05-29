0a62665df7f4e31e98f8e63fd82e3ee3dfe354d5
RAR\cristian_mar <cristian.mardare@rarom.ro>
2023-05-29 17:19:34 +0300

    stage 29.05.2023

---
+++ src/app/modules/echipamente-service/export-echipamente-list/export-echipamente-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-export-echipamente-list',
+  templateUrl: './export-echipamente-list.component.html',
+  styleUrls: ['./export-echipamente-list.component.scss']
+})
+export class ExportEchipamenteListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
